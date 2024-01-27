56cf0202e7938073837e1f2a9bf507e96fecee82
meganjchiang <mjchiang@uw.edu>
2024-01-27 11:49:30 -0800

    start working on app

--- winfo24/app/layout.js
+++ winfo24/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Montserrat, Merriweather } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ weight: '700', subsets: ["latin"] });
+const merriweather = Merriweather({ weight: '400', subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
+      {/* <body className={merriweather.className}>{children}</body> */}
     </html>
   );
 }
--- winfo24/tailwind.config.js
+++ winfo24/tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'black': '#333637',
+        'white': '#F5F5F5',
+        'light-yellow': '#FFEBB9',
+        'med-yellow': '#FFD063',
+        'dark-yellow': '#FFB300',
+        'dark-blue': '#00A9FF',
+        'med-blue': '#89CFF3',
+        'light-blue': '#A0E9FF',
+        'lightest-blue': '#CDF5FD',
+      },
     },
   },
   plugins: [],
